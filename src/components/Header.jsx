7fc94574265054805f2357ca08a3aa48351a272c
Kiara Johnson <kiarajohnson91@gmail.com>
2023-12-13 20:27:44 -0600

    changed navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import Navigation from "./Navigation";
-import Typewriter from "./Typewriter";
+// import Typewriter from "./Typewriter";
 
 function Header({ currentPage, handlePageChange }) {
   return (
     <header>
       <div>
-        <h1>
+        {/* <h1>
           Hi, I'm Kiara!{" "}
           <Typewriter text="Full Stack Web Developer" delay={100} infinite />
-        </h1>
+        </h1> */}
       </div>
       <Navigation
         currentPage={currentPage}
