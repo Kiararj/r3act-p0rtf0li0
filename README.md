da81e994c90d4c65e2b3c7118dc9ec2da3db5360
Kiara Johnson <kiarajohnson91@gmail.com>
2023-12-28 00:00:47 -0600

    added more styling

--- README.md
+++ README.md
@@ -1,32 +1,4 @@
 # React Portfolio
 
-## User Story
 
-AS AN employer looking for candidates with experience building single-page applications
-I WANT to view a potential employee's deployed React portfolio of work samples
-SO THAT I can assess whether they're a good candidate for an open position
-
-## Projects
-
-For each project you feature in your portfolio, you should include the following:
-
-An image of the deployed application (either a GIF or a screenshot)
-
-The title of the project
-
-A link to the deployed application
-
-A link to the corresponding GitHub repository
-
-## Design
-
-As with the previous portfolio Challenge, remember that "good" design is subjective; however, your site should look polished. Here are a few guidelines on what that means:
-
-Use mobile-first design.
-
-Choose a color palette so that your site doesn't just look like the default Bootstrap theme or an unstyled HTML site. Look into resources like Coolors. Links to an external site. or another color scheme generator to help you create something that will stand out.
-
-Make sure the font size is large enough to read and that the colors don't cause eye strain.
-
-Consider using animations and React component libraries. Note that this will not affect your grade, but it may impact how potential employers gauge your knowledge.
 
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,4 @@
-import Typewriter from "./Typewriter";
+import Typewriter from './Typewriter';
 
 function Navigation({ currentPage, handlePageChange }) {
 
@@ -7,69 +7,69 @@ function Navigation({ currentPage, handlePageChange }) {
   };
   
   return (
-    <nav class="navbar navbar-expand-lg" style={navStyle}>
-      <div class="container-fluid">
-        <h1 className="navbar-brand">
-          Hi, I'm Kiara! {"👋🏾 "}
-          <Typewriter text="Full Stack Web Developer" delay={100} infinite />
+    <nav class='navbar navbar-expand-lg' style={navStyle}>
+      <div class='container-fluid'>
+        <h1 className='navbar-brand'>
+          Hi, I'm Kiara! {'👋🏾 '}
+          <Typewriter text='Full Stack Web Developer' delay={100} infinite />
         </h1>
         <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+          class='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarNav'
+          aria-controls='navbarNav'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
         >
-          <span class="navbar-toggler-icon"></span>
+          <span class='navbar-toggler-icon'></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
+        <div class='collapse navbar-collapse' id='navbarNav'>
+          <ul class='navbar-nav ms-auto'>
+            <li class='nav-item'>
               <a
-                href="#aboutme"
-                onClick={() => handlePageChange("AboutMe")}
+                href='#aboutme'
+                onClick={() => handlePageChange('AboutMe')}
                 // This is a conditional (ternary) operator that checks to see if the current page is "AboutMe"
                 // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
                 className={
-                  currentPage === "AboutMe" ? "nav-link active" : "nav-link"
+                  currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link'
                 }
               >
                 About Me
               </a>
             </li>
-            <li class="nav-item">
+            <li class='nav-item'>
               <a
-                href="#portfolio"
-                onClick={() => handlePageChange("Portfolio")}
+                href='#portfolio'
+                onClick={() => handlePageChange('Portfolio')}
                 // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
                 className={
-                  currentPage === "Portfolio" ? "nav-link active" : "nav-link"
+                  currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'
                 }
               >
                 Portfolio
               </a>
             </li>
-            <li class="nav-item">
+            <li class='nav-item'>
               <a
-                href="#contact"
-                onClick={() => handlePageChange("Contact")}
+                href='#contact'
+                onClick={() => handlePageChange('Contact')}
                 // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
                 className={
-                  currentPage === "Contact" ? "nav-link active" : "nav-link"
+                  currentPage === 'Contact' ? 'nav-link active' : 'nav-link'
                 }
               >
                 Contact
               </a>
             </li>
-            <li class="nav-item">
+            <li class='nav-item'>
               <a
-                href="#resume"
-                onClick={() => handlePageChange("Resume")}
+                href='#resume'
+                onClick={() => handlePageChange('Resume')}
                 // Check to see if the currentPage is `Resume`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
                 className={
-                  currentPage === "Resume" ? "nav-link active" : "nav-link"
+                  currentPage === 'Resume' ? 'nav-link active' : 'nav-link' 
                 }
               >
                 Resume
